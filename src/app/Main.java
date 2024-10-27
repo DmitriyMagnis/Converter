4bd65515d374264fa52dc5d84c3bb4c1458b882e
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-10-27 10:59:37 +0100

    ADD/kilometres to miles method

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,23 @@
 package app;
 
 public class Main {
+
+    private static final double CONV_K_M = 0.621371;
+
     public static void main(String[] args) {
         System.out.println("App for measures converting." );
+
+        double kms = 5;
+
+        double miles = convKmsToMiles(kms);
+
+        System.out.println("Result is " + miles);
+
+
     }
+    private static double convKmsToMiles(double kms) {
+        return kms * CONV_K_M;
+    }
+
+
 }
